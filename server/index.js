370734e7b28d0a76fcdd11d910c2fc7c970e8eae
= <=>
2025-06-24 09:45:04 +0800

    feature:project init

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello from Docker!")
+})
+
+app.listen(7777, () => {
+  console.log("Server is running on port 7777")
+})
